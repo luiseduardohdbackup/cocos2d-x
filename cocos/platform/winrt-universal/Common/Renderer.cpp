887065bc97f0606f55f5b700dce2f72ec217b28a
Dale Stammen <dalestam@microsoft.com>
2014-06-26 16:24:34 -0700

    univeral app build

---
+++ cocos/platform/winrt-universal/Common/Renderer.cpp
@@ -0,0 +1,155 @@
+﻿#include "pch.h"
+#include "Renderer.h"
+#include "AngleHelper.h"
+#include "cocos2d.h"
+#include "CCApplication.h"
+#include "CCGLView.h"
+#include "AppDelegate.h"
+
+using namespace DirectX;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Core;
+USING_NS_CC;
+
+
+namespace AngleApp
+{
+
+
+// Loads vertex and pixel shaders from files and instantiates the cube geometry.
+Renderer::Renderer(const std::shared_ptr<AngleApp::DeviceResources>& deviceResources) 
+    : m_loadingComplete(false)
+    , m_setupComplete(false)
+    , m_deviceResources(deviceResources)
+{
+    mApp = new AppDelegate();
+    CreateDeviceDependentResources();
+}
+
+// Initializes view parameters when the window size changes.
+void Renderer::CreateWindowSizeDependentResources()
+{
+    m_deviceResources->aquireContext();
+    auto outputSize = m_deviceResources->GetOutputSize();
+    auto logicalSize = m_deviceResources->GetLogicalSize();
+    auto director = cocos2d::Director::getInstance();
+
+
+    if (!m_setupComplete)
+    {
+        GLView* glview = GLView::create("Test Cpp");
+        glview->Create(logicalSize.Width, logicalSize.Height, m_deviceResources->GetCurrentOrientation());
+        director->setOpenGLView(glview);
+        CCApplication::getInstance()->run();
+    }
+    else
+    {
+        cocos2d::GL::invalidateStateCache();
+        cocos2d::ShaderCache::getInstance()->reloadDefaultGLPrograms();
+        cocos2d::DrawPrimitives::init();
+        cocos2d::VolatileTextureMgr::reloadAllTextures();
+        cocos2d::EventCustom foregroundEvent(EVENT_COME_TO_FOREGROUND);
+        director->getEventDispatcher()->dispatchEvent(&foregroundEvent);
+        cocos2d::Application::getInstance()->applicationWillEnterForeground();
+        director->setGLDefaultValues();
+    }
+    m_setupComplete = true;
+    m_deviceResources->releaseContext();
+
+}
+
+// Called once per frame, rotates the cube and calculates the model and view matrices.
+void Renderer::Update(AngleApp::StepTimer const& timer)
+{
+
+}
+
+void Renderer::OnPointerPressed(PointerEventArgs^ e)
+{
+    //window->OnPointerPressed(e);
+}
+
+void Renderer::OnPointerMoved(PointerEventArgs^ e)
+{
+    //window->OnPointerMoved(e);
+}
+
+void Renderer::OnPointerReleased(PointerEventArgs^ e)
+{
+    //window->OnPointerReleased(e);
+}
+
+void Renderer::OnKeyPressed(Windows::UI::Core::KeyEventArgs^ e)
+{
+	//window->OnKeyPressed(e);
+}
+
+void Renderer::OnKeyReleased(Windows::UI::Core::KeyEventArgs^ e)
+{
+	//window->OnKeyReleased(e);
+}
+
+
+
+void Renderer::AddPointerEvent(PointerEventType type, PointerEventArgs^ args)
+{
+    std::lock_guard<std::mutex> guard(mMutex);
+	std::shared_ptr<PointerEvent> e(new PointerEvent(type, args));
+    mInputEvents.push(e);
+}
+
+void Renderer::AddKeyboardEvent(KeyboardEventType type, Windows::UI::Core::KeyEventArgs^ args)
+{
+	std::lock_guard<std::mutex> guard(mMutex);
+	std::shared_ptr<KeyboardEvent> e(new KeyboardEvent(type, args));
+	mInputEvents.push(e);
+}
+
+
+
+void Renderer::ProcessEvents()
+{
+    std::lock_guard<std::mutex> guard(mMutex);
+
+    while (!mInputEvents.empty())
+    {
+        InputEvent* e = mInputEvents.front().get();
+        e->execute(this);
+        mInputEvents.pop();
+    }
+}
+
+
+// Renders one frame using the vertex and pixel shaders.
+void Renderer::Render()
+{
+    if (m_setupComplete)
+	{
+        m_deviceResources->aquireContext();
+
+        ProcessEvents();
+#if 0
+        glClearColor(1.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+#else
+        GLView* glview = GLView::sharedOpenGLView();
+        glview->Render();
+
+#endif
+        m_deviceResources->Present();
+
+        m_deviceResources->releaseContext();
+    }
+}
+
+void Renderer::CreateDeviceDependentResources()
+{
+
+}
+
+void Renderer::ReleaseDeviceDependentResources()
+{
+	m_loadingComplete = false;
+}
+
+}
---
+++ tests/cpp-empty-test/proj.winrt-universal/cpp-empty-test/cpp-empty-test.WindowsPhone/MainPage.xaml.cpp
@@ -0,0 +1,189 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace cpp_empty_test;
+
+using namespace AngleApp;
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Graphics::Display;
+using namespace Windows::System::Threading;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Input;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace concurrency;
+
+MainPage::MainPage() :
+	m_windowVisible(true),
+	m_coreInput(nullptr)
+{
+	InitializeComponent();
+
+	// Register event handlers for page lifecycle.
+	CoreWindow^ window = Window::Current->CoreWindow;
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &MainPage::OnVisibilityChanged);
+
+	DisplayInformation^ currentDisplayInformation = DisplayInformation::GetForCurrentView();
+
+	currentDisplayInformation->DpiChanged +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &MainPage::OnDpiChanged);
+
+	currentDisplayInformation->OrientationChanged +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &MainPage::OnOrientationChanged);
+
+	DisplayInformation::DisplayContentsInvalidated +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &MainPage::OnDisplayContentsInvalidated);
+
+	swapChainPanel->CompositionScaleChanged += 
+		ref new TypedEventHandler<SwapChainPanel^, Object^>(this, &MainPage::OnCompositionScaleChanged);
+
+	swapChainPanel->SizeChanged +=
+		ref new SizeChangedEventHandler(this, &MainPage::OnSwapChainPanelSizeChanged);
+
+	// At this point we have access to the device. 
+	// We can create the device-dependent resources.
+	m_deviceResources = std::make_shared<AngleApp::DeviceResources>();
+	m_deviceResources->SetSwapChainPanel(swapChainPanel);
+
+	// Register our SwapChainPanel to get independent input pointer events
+	auto workItemHandler = ref new WorkItemHandler([this] (IAsyncAction ^)
+	{
+		// The CoreIndependentInputSource will raise pointer events for the specified device types on whichever thread it's created on.
+		m_coreInput = swapChainPanel->CreateCoreIndependentInputSource(
+			Windows::UI::Core::CoreInputDeviceTypes::Mouse |
+			Windows::UI::Core::CoreInputDeviceTypes::Touch |
+			Windows::UI::Core::CoreInputDeviceTypes::Pen
+			);
+
+		// Register for pointer events, which will be raised on the background thread.
+		m_coreInput->PointerPressed += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &MainPage::OnPointerPressed);
+		m_coreInput->PointerMoved += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &MainPage::OnPointerMoved);
+		m_coreInput->PointerReleased += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &MainPage::OnPointerReleased);
+
+		// Begin processing input messages as they're delivered.
+		m_coreInput->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+	});
+
+	// Run task on a dedicated high priority background thread.
+	m_inputLoopWorker = ThreadPool::RunAsync(workItemHandler, WorkItemPriority::High, WorkItemOptions::TimeSliced);
+
+    m_main = std::unique_ptr<AppMain>(new AppMain(m_deviceResources));
+	m_main->StartRenderLoop();
+}
+
+MainPage::~MainPage()
+{
+	// Stop rendering and processing events on destruction.
+	m_main->StopRenderLoop();
+	m_coreInput->Dispatcher->StopProcessEvents();
+}
+
+// Saves the current state of the app for suspend and terminate events.
+void MainPage::SaveInternalState(IPropertySet^ state)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->Trim();
+
+	// Stop rendering when the app is suspended.
+	m_main->StopRenderLoop();
+
+	// Put code to save app state here.
+}
+
+// Loads the current state of the app for resume events.
+void MainPage::LoadInternalState(IPropertySet^ state)
+{
+	// Put code to load app state here.
+
+	// Start rendering when the app is resumed.
+	m_main->StartRenderLoop();
+}
+
+// Window event handlers.
+
+void MainPage::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	m_windowVisible = args->Visible;
+	if (m_windowVisible)
+	{
+		m_main->StartRenderLoop();
+	}
+	else
+	{
+		m_main->StopRenderLoop();
+	}
+}
+
+// DisplayInformation event handlers.
+
+void MainPage::OnDpiChanged(DisplayInformation^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->SetDpi(sender->LogicalDpi);
+	m_main->CreateWindowSizeDependentResources();
+}
+
+void MainPage::OnOrientationChanged(DisplayInformation^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->SetCurrentOrientation(sender->CurrentOrientation);
+	m_main->CreateWindowSizeDependentResources();
+}
+
+
+void MainPage::OnDisplayContentsInvalidated(DisplayInformation^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->ValidateDevice();
+}
+
+void MainPage::OnPointerPressed(Object^ sender, PointerEventArgs^ e)
+{
+
+}
+
+void MainPage::OnPointerMoved(Object^ sender, PointerEventArgs^ e)
+{
+
+}
+
+void MainPage::OnPointerReleased(Object^ sender, PointerEventArgs^ e)
+{
+
+}
+
+void MainPage::OnCompositionScaleChanged(SwapChainPanel^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->SetCompositionScale(sender->CompositionScaleX, sender->CompositionScaleY);
+	m_main->CreateWindowSizeDependentResources();
+}
+
+void MainPage::OnSwapChainPanelSizeChanged(Object^ sender, SizeChangedEventArgs^ e)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	m_deviceResources->SetLogicalSize(e->NewSize);
+	m_main->CreateWindowSizeDependentResources();
+}
+
+// Uncomment this if using the app bar in your phone application.
+// Called when the app bar button is clicked.
+//void MainPage::AppBarButton_Click(Object^ sender, RoutedEventArgs^ e)
+//{
+//	// Use the app bar if it is appropriate for your app. Design the app bar, 
+//	// then fill in event handlers (like this one).
+//}
