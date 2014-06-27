28e885ecfcc1241599c7d2ec657bdd6b343dfef1
Dale Stammen <dalestam@microsoft.com>
2014-06-27 09:29:44 -0700

    added winrt universal app project files

---
+++ tests/cpp-tests/proj.winrt-universal/cpp-tests/cpp-tests.Windows/MainPage.xaml.h
@@ -0,0 +1,55 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+#include "DeviceResources.h"
+#include "AppMain.h"
+
+namespace cpp_empty_test
+{
+	public ref class MainPage sealed
+	{
+	public:
+        MainPage();
+        virtual ~MainPage();
+
+		void SaveInternalState(Windows::Foundation::Collections::IPropertySet^ state);
+		void LoadInternalState(Windows::Foundation::Collections::IPropertySet^ state);
+
+	private:
+		// XAML low-level rendering event handler.
+		void OnRendering(Platform::Object^ sender, Platform::Object^ args);
+
+		// Window event handlers.
+		void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+
+		// DisplayInformation event handlers.
+		void OnDpiChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnOrientationChanged(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+		void OnDisplayContentsInvalidated(Windows::Graphics::Display::DisplayInformation^ sender, Platform::Object^ args);
+
+		// Other event handlers.
+		void AppBarButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void OnCompositionScaleChanged(Windows::UI::Xaml::Controls::SwapChainPanel^ sender, Object^ args);
+		void OnSwapChainPanelSizeChanged(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventArgs^ e);
+
+		// Track our independent input on a background worker thread.
+		Windows::Foundation::IAsyncAction^ m_inputLoopWorker;
+		Windows::UI::Core::CoreIndependentInputSource^ m_coreInput;
+
+		// Independent input handling functions.
+		void OnPointerPressed(Platform::Object^ sender, Windows::UI::Core::PointerEventArgs^ e);
+		void OnPointerMoved(Platform::Object^ sender, Windows::UI::Core::PointerEventArgs^ e);
+		void OnPointerReleased(Platform::Object^ sender, Windows::UI::Core::PointerEventArgs^ e);
+
+		// Resources used to render the DirectX content in the XAML page background.
+		std::shared_ptr<AngleApp::DeviceResources> m_deviceResources;
+        std::unique_ptr<AngleApp::AppMain> m_main;
+		bool m_windowVisible;
+	};
+}
+
